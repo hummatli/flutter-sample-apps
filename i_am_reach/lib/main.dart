7aefa9fc2493189afb172618aea498ba9647dd79
Sattar Hummatli <shummatli@Sattars-MacBook-Pro.local>
2020-02-13 14:43:03 +0400

    I am rich app - 2

--- i_am_reach/lib/main.dart
+++ i_am_reach/lib/main.dart
@@ -12,10 +12,11 @@ void main() => runApp(
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: Image(
-                image: NetworkImage(
-                    'https://helpx.adobe.com/content/dam/help/en/stock/how-to'
-                    '/visual-reverse-image-search/jcr_content/main-pars/image'
-                    '/visual-reverse-image-search-v2_intro.jpg'),
+//                image: NetworkImage(
+//                    'https://helpx.adobe.com/content/dam/help/en/stock/how-to'
+//                    '/visual-reverse-image-search/jcr_content/main-pars/image'
+//                    '/visual-reverse-image-search-v2_intro.jpg'),
+                image: AssetImage('images/diamond.png'),
               ),
             ),
           ),
