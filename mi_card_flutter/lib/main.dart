5f6f7612a01fa7a88a7075a0e189b4d41e9fcbd2
Sattar Hummatli <shummatli@Sattars-MacBook-Pro.local>
2020-02-18 11:28:22 +0400

    Change main colors

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -11,71 +11,80 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.blue,
         body: SafeArea(
-          child: Column(children: <Widget>[
-            CircleAvatar(
-              radius: 50.0,
-              backgroundImage: AssetImage('images/profile.jpeg'),
-            ),
-            Text(
-              'Səttar Hümmətli',
-              style: TextStyle(
-                fontFamily: 'Pacifico',
-                color: Colors.white,
-                fontSize: 40.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              'FLUTTER DEVELOPER',
-              style: TextStyle(
-                fontFamily: 'SourceSansPro',
-                color: Colors.teal.shade100,
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 3.0,
-              ),
-            ),
-            Card(
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 32.0),
-//              padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-              child: ListTile(
-                leading: Icon(
-                  Icons.phone,
-                  color: Colors.teal,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/profile.jpeg'),
                 ),
-                title: Text(
-                  '+994 50 646 46 46',
+                Text(
+                  'Səttar Hümmətli',
                   style: TextStyle(
-                    fontFamily: 'SourceSansPro',
-                    fontSize: 20.0,
-                    color: Colors.teal.shade900,
+                    fontFamily: 'Pacifico',
+                    color: Colors.white,
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-            ),
-            Card(
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 32.0),
-//              padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-              child: ListTile(
-                leading: Icon(
-                  Icons.email,
-                  color: Colors.teal,
-                ),
-                title: Text(
-                  'settar@gmail.com',
+                Text(
+                  'FLUTTER DEVELOPER',
                   style: TextStyle(
                     fontFamily: 'SourceSansPro',
+                    color: Colors.white70,
                     fontSize: 20.0,
-                    color: Colors.teal.shade900,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 3.0,
                   ),
                 ),
-              ),
-            ),
-          ]),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.white70,
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 32.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.blue,
+                    ),
+                    title: Text(
+                      '+994 50 646 46 46',
+                      style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
+                        color: Colors.blue.shade900,
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 32.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.blue,
+                    ),
+                    title: Text(
+                      'settar@gmail.com',
+                      style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
+                        color: Colors.blue.shade900,
+                      ),
+                    ),
+                  ),
+                ),
+              ]),
         ),
       ),
     );
